94509fc2d8ba5e321b71c1c8b6eb3831d5b7e505
lucsamsonpro <luc.samson.pro@gmail.com>
2025-10-24 11:20:33 +0200

    Work on exercice#2

--- exercicesC#/Assets/Exercices/Exercice2.cs
+++ exercicesC#/Assets/Exercices/Exercice2.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Exercice2 : MonoBehaviour
@@ -11,25 +12,79 @@ public class Exercice2 : MonoBehaviour
     // Code à compléter (ne rien modifier avant cette ligne)
 
     public SpriteRenderer spriteRenderer;
-    public Color color;
+    public int randomColor;
+    public float timePassed = 0f;
 
     public void Start()
     {
         this.spriteRenderer = GetComponent<SpriteRenderer>();
-        this.spriteRenderer.color = Color.blue;
+        randomColor = 0;
     }
-    private void Update()
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        GenerateRandomColor();
+        ChangeToBlue();
+        ChangeToRed();
+        ChangeToGreen();
+        ChangeToYellow();
+        ChangeToWhite();
+        ChangeToBlack();
+
+
+    }
+
+    void GenerateRandomColor()
+    {
+        timePassed += Time.deltaTime;
+        if (timePassed > 1f)
         {
-            if(this.spriteRenderer.color = Color.blue)
-            { 
-                this.spriteRenderer.color = Color.red
-             }
-            else
-            { this.spriteRenderer.color = Color.blue;}
+            randomColor = Random.Range(0, 5);
+            Debug.Log("RandomColor = " + randomColor);
+            timePassed = 0f;
         }
     }
+
+
+
+    void ChangeToBlue()
+    {
+        if (randomColor == 0)
+            this.spriteRenderer.color = Color.blue;
+    }
+
+    void ChangeToRed()
+    {
+        if (randomColor == 1)
+            this.spriteRenderer.color = Color.red;
+    }
+
+    void ChangeToGreen()
+    {
+        if (randomColor == 2)
+            this.spriteRenderer.color = Color.green;
+    }
+
+    void ChangeToYellow()
+    { 
+    if (randomColor == 3)
+    this.spriteRenderer.color = Color.yellow;
+    }
+
+    void ChangeToWhite()
+    { 
+    if (randomColor == 4)
+    this.spriteRenderer.color = Color.white;
+    }
+
+    void ChangeToBlack()
+    { 
+    if (randomColor == 5)
+    this.spriteRenderer.color = Color.black;
+    }
+
+
+
+
 
     // Fin du code à compléter (ne rien modifier après cette ligne)
 }
